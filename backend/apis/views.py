edca45d9ff30b42422da577a3103dc57d88b68c3
Alexander Boneham <alexboneham@gmail.com>
2022-08-17 15:45:37 -0400

    move listing of library function to index route

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -5,14 +5,11 @@ from .models import Song
 
 
 def index(request):
-    return HttpResponse('Made it to api index route!')
-
-def library(request):
     # Returns a list of all songs in the song libary
     songs = Song.objects.all()
     return render(request, 'index.html', {
         'songs': songs
-    })
+    })    
 
 def song(request, song_id):
     song = Song.objects.get(id=song_id)
@@ -20,8 +17,6 @@ def song(request, song_id):
         'song': song
     })
 
-def new_song(request):
-    return HttpResponse('New song response')
 
 
 # def login_view(request):
