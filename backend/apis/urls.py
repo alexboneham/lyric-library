60eace5a2568eac1ca65c9f6fc6c3829ecc3e1b5
Alexander Boneham <alexboneham@gmail.com>
2022-08-26 11:16:54 -0400

    build rough functionality for adding new song through template

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new', views.new_song, name='new'),
     path('<int:song_id>/edit', views.edit_song, name='edit'),
-    path('<int:song_id>', views.song, name='song'),
+    path('<int:song_id>', views.show_song, name='song'),
 ]
--- backend/apis/views.py
+++ backend/apis/views.py
@@ -3,33 +3,44 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Song
 from .utils import song_to_dict
+from .forms.new_song_form import NewSongForm
 
 
 def index(request):
     # Returns a list of all songs in the song libary
     songs_querySet = Song.objects.all()
-    songs = {}
-    for song in songs_querySet:
-        songs[song.id] = song_to_dict(song)
+    # songs = {}
+    # for song in songs_querySet:
+    #     songs[song.id] = song_to_dict(song)
     # return JsonResponse(songs)    
     return render(request, 'index.html', {
         'songs': songs_querySet
     })
 
 
-def song(request, song_id):
+def show_song(request, song_id):
     try:
         song = Song.objects.get(id=song_id)
         return render(request, 'song.html', {
             'song': song,
         })
-    except:
-        return HttpResponse('Song does not exist')
+    except Exception as e:
+        return HttpResponse(e)
 
     # return JsonResponse(song_to_dict(song))
 
 def new_song(request):
-    return HttpResponse('Made it to new song view')
+    if request.method == 'POST':
+        form = NewSongForm(request.POST)
+        if form.is_valid():
+            new_title = form.cleaned_data['title']
+            # Send song title to Genius API
+            # TODO
+            return HttpResponse(f'Made it to new song POST view with {new_title}')
+        else:
+            return HttpResponse('Form not valid')
+        
+    return render(request, 'new.html', {})
 
 
 def edit_song(request, song_id):
