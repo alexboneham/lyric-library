bf2fc67b5acff61deb48dc8e4d40bbb7ac96b34f
Alexander Boneham <alexboneham@gmail.com>
2022-09-06 12:50:14 -0400

    change url patterns for library routes. Fix links to library from index. Add white-space style attribute to lyrics paragraph to preserve correct formatting

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('library', views.load_library, name='library'),
+    path('library/<int:song_id>/edit', views.edit_song, name='edit'),
+    path('library/<int:song_id>', views.show_song, name='song'),
     path('search', views.search_genius, name='search'),
-    path('<int:song_id>/edit', views.edit_song, name='edit'),
-    path('<int:song_id>', views.show_song, name='song'),
 ]
