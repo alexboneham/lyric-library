f37f23ed525ef1b32962302c8ac5052f29b4bfb3
Alexander Boneham <alexboneham@gmail.com>
2022-09-26 14:35:35 -0400

    show library song via its route. Display basic song info, lyrics, etc

--- frontend/src/routes/library-item/library-item.component.jsx
+++ frontend/src/routes/library-item/library-item.component.jsx
@@ -1,11 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import './library-item.styles.scss';
 
 const LibraryItem = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [song, setSong] = useState({});
 
-    return (
-        <div>Library item {id}</div>
-    )
-}
+  useEffect(() => {
+    console.log('Inside fetch useEffect...')
+    const fetchSong = async () => {
+      const res = await (await fetch(`http://localhost:8000/library/${id}`)).json();
+      setSong(res);
+    };
+    fetchSong()
+  }, []);
+
+  return (
+    <div className='library-item-container'>
+      <h1 className='title'>{song.title}</h1>
+      <h2 className='artist'>by {song.artist}</h2>
+      <p className='lyrics'>{song.lyrics}</p>
+    </div>
+  )
+};
 
 export default LibraryItem;
