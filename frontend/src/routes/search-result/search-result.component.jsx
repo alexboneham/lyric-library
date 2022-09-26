d9b4ac3580bd2353ad881117039fbc6c04eadbde
Alexander Boneham <alexboneham@gmail.com>
2022-09-26 11:24:39 -0400

    add try/catch error handling to search results fetch

--- frontend/src/routes/search-result/search-result.component.jsx
+++ frontend/src/routes/search-result/search-result.component.jsx
@@ -6,29 +6,35 @@ import './search-result.styles.scss';
 const SearchResult = () => {
   const { id } = useParams();
   const [song, setSong] = useState(undefined);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     // API call for song details
-    fetch(`http://localhost:8000/search/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setSong(data);
-        console.log('useEffect run')
-      });
+    try {
+      fetch(`http://localhost:8000/search/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setSong(data);
+          console.log('useEffect run');
+        });
+    } catch (e) {
+      console.log(`Error: ${e}`);
+      setIsError(true);
+    }
   }, [id]);
-
 
   return (
     <div className="search-result-container">
+      {isError && <div>Something went wrong...</div>}
       {song && (
         <Fragment>
-        <h1 className='title'>{song.title}</h1>
-        <h3 className='artist-name'>by {song.artist}</h3>
-        <p className='lyrics'>{song.lyrics}</p>
+          <h1 className="title">{song.title}</h1>
+          <h3 className="artist-name">by {song.artist}</h3>
+          <p className="lyrics">{song.lyrics}</p>
         </Fragment>
-      )}    
+      )}
     </div>
-  )
+  );
 };
 
 export default SearchResult;
