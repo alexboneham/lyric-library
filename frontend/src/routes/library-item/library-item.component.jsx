94ea1ced156883bbee33eb22028d7f03e7c179eb
Alexander Boneham <alexboneham@gmail.com>
2022-09-26 20:09:18 -0400

    add song info to library song page

--- frontend/src/routes/library-item/library-item.component.jsx
+++ frontend/src/routes/library-item/library-item.component.jsx
@@ -12,16 +12,22 @@ const LibraryItem = () => {
       const res = await (await fetch(`http://localhost:8000/library/${id}`)).json();
       setSong(res);
     };
-    fetchSong()
+    fetchSong();
   }, [id]);
 
   return (
-    <div className='library-item-container'>
-      <h1 className='title'>{song.title}</h1>
-      <h2 className='artist'>by {song.artist}</h2>
-      <p className='lyrics'>{song.lyrics}</p>
+    <div className="library-item-container">
+      <div className="song-info">
+        <h1 className="title">{song.title}</h1>
+        <h2 className="artist">by {song.artist}</h2>
+        <img src={song.thumbnail_url} alt={song.full_title} />
+        <p className='description'>{song.description}</p>
+      </div>
+      <div className="lyrics-container">
+        <p className="lyrics">{song.lyrics}</p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default LibraryItem;
