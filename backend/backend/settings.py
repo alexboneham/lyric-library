8f5cb46c8bda2c5be083d87efe5917b596ac633a
Alexander Boneham <alexboneham@gmail.com>
2022-10-11 16:19:57 -0400

    change settings to allow cors credentials and csrf trusted origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,8 +131,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CSRF_COOKIE_HTTPONLY = False
 # SESSION_COOKIE_HTTPONLY = True
 
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+SESSION_COOKIE_SAMESITE = None
+SESSION_COOKIE_SECURE = True
