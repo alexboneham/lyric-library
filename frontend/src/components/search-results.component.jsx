10995989593a4c333693abdad04e115118487cde
Alexander Boneham <alexboneham@gmail.com>
2022-10-14 12:42:19 -0400

    styled search results page to include images

--- frontend/src/components/search-results.component.jsx
+++ frontend/src/components/search-results.component.jsx
@@ -2,17 +2,37 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Nav from 'react-bootstrap/Nav';
+import Stack from 'react-bootstrap/Stack';
+import Image from 'react-bootstrap/Image';
 
 const SearchResults = ({ songs }) => {
   return (
-    <Container className='text-center p-3'>
+    <Container className="d-flex flex-column align-items-center">
       <h2>Search Results...</h2>
-      <ListGroup variant="flush">
-        {songs.map((song) => {
+      <ListGroup variant="flush" className="align-items-start">
+        {songs.slice(0, 5).map((song) => {
+          // N.B. limiting results to 5 here. Could do this via pagination too...
           return (
-            <LinkContainer to={`/search/${song.result.id}`} key={song.result.id} style={{ cursor: 'pointer' }}>
-              <ListGroup.Item className='text-decoration-underline'>{song.result.full_title}</ListGroup.Item>
-            </LinkContainer>
+            <ListGroup.Item key={song.result.id}>
+              <Nav>
+                <Nav.Item>
+                  <LinkContainer to={`/search/${song.result.id}`}>
+                    <Nav.Link>
+                      <Container>
+                        <Stack direction="horizontal" gap={3}>
+                          <Image src={song.result.song_art_image_url} fluid width={171} height={171} rounded />
+                          <Stack gap={1} className="my-auto">
+                            <span>{song.result.title}</span>
+                            <span className="text-muted">by {song.result.artist_names}</span>
+                          </Stack>
+                        </Stack>
+                      </Container>
+                    </Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+              </Nav>
+            </ListGroup.Item>
           );
         })}
       </ListGroup>
@@ -21,3 +41,5 @@ const SearchResults = ({ songs }) => {
 };
 
 export default SearchResults;
+
+// {song.result.full_title}
