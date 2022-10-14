ecba813181186b5ade40cf356c7e734290b19f06
Alexander Boneham <alexboneham@gmail.com>
2022-10-14 13:14:36 -0400

    add more bootstrap styling to homepage

--- frontend/src/routes/home.component.jsx
+++ frontend/src/routes/home.component.jsx
@@ -1,26 +1,38 @@
-import { Link } from 'react-router-dom';
-
 import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Home = ({ title, message }) => (
-  <Container className="p-3 mt-3 text-center shadow-lg rounded ">
-    <h1 className="display-4">{title}</h1>
-    <p>{message}</p>
+  <Container className="p-5 mt-5 shadow-lg rounded ">
+    <Container className="text-center px-5 pb-2">
+      <h1 className="display-4">{title}</h1>
+      <p>{message}</p>
+    </Container>
     <hr />
-    <h2>Usage:</h2>
-    <Container className="w-75">
-      <ListGroup variant="flush">
-        <ListGroup.Item>
-          <Link to={'/search'}>Search</Link> for a song by title or artist
-        </ListGroup.Item>
-        <ListGroup.Item>
-          Save a song to your <Link to={'/library'}>Library</Link> for fast access and more features
-        </ListGroup.Item>
-        <ListGroup.Item>
-          Create <Link to={'/setlists'}>Setlists</Link> and add songs
-        </ListGroup.Item>
-      </ListGroup>
+    <Container className="d-flex flex-column align-items-center">
+      <h2>Usage:</h2>
+      <Nav className='flex-column align-items-center'>
+        <Nav.Item className="mb-2">
+          <LinkContainer to={'/search'}>
+            <Nav.Link className="d-inline-flex p-0">Search</Nav.Link>
+          </LinkContainer>{' '}
+          for a song by title or artist
+        </Nav.Item>
+        <Nav.Item className="mb-2">
+          Save a song to your{' '}
+          <LinkContainer to={'/library'}>
+            <Nav.Link className="d-inline-flex p-0">library</Nav.Link>
+          </LinkContainer>{' '}
+          for fast access and more features
+        </Nav.Item>
+        <Nav.Item className="mb-2">
+          Create{' '}
+          <LinkContainer to={'/setlists'}>
+            <Nav.Link className="d-inline-flex p-0">setlists</Nav.Link>
+          </LinkContainer>{' '}
+          and add songs
+        </Nav.Item>
+      </Nav>
     </Container>
   </Container>
 );
