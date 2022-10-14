32677b4b50194004e1164c7fdc6eeb23e0d2b779
Alexander Boneham <alexboneham@gmail.com>
2022-10-14 11:39:37 -0400

    style edit text area form with bootstrap

--- frontend/src/components/song-item/song-item.component.jsx
+++ frontend/src/components/song-item/song-item.component.jsx
@@ -5,13 +5,12 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/esm/Button';
+import Stack from 'react-bootstrap/Stack';
 
 const SongItem = ({ song, description, thumbnail, buttonProps, actionProps }) => {
-  const {
-    editOpen = undefined,
-    handleEditSubmit = undefined,
-    handleEditChange = undefined,
-  } = actionProps;
+  const { editOpen = undefined, handleEditSubmit = undefined, handleEditChange = undefined } = actionProps;
 
   if (thumbnail) {
     song['thumbnail_url'] = thumbnail;
@@ -29,12 +28,22 @@ const SongItem = ({ song, description, thumbnail, buttonProps, actionProps }) =>
         </Col>
         <Col lg={1} />
         <Col lg={5} className="p-5 border rounded">
-          <EditButtons buttonProps={buttonProps} song={song}/>
+          <EditButtons buttonProps={buttonProps} song={song} />
           {editOpen ? (
-            <form onSubmit={handleEditSubmit}>
-              <textarea defaultValue={song.lyrics} cols={50} rows={50} onChange={handleEditChange} />
-              <button type="submit">Submit</button>
-            </form>
+            <Form onSubmit={handleEditSubmit} className="mt-3">
+              <Form.Control
+                as={'textarea'}
+                defaultValue={song.lyrics}
+                cols={50}
+                rows={50}
+                onChange={handleEditChange}
+                className="text-center"
+              />
+              <Stack gap={2} className="col-md-5 mx-auto mt-2">
+                <Button variant="secondary" type='submit'>Save changes</Button>
+                <Button variant="outline-secondary" onClick={buttonProps.editButtonClick}>Cancel</Button>
+              </Stack>
+            </Form>
           ) : (
             <p className="mt-3 text-center" style={{ whiteSpace: 'pre-line' }}>
               {song.lyrics}
