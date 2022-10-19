7883183060bf9ae18c48cc352ecfddb49b7c415c
Alexander Boneham <alexboneham@gmail.com>
2022-10-19 16:51:38 -0400

    style 404 not found page

--- frontend/src/routes/not-found.component.jsx
+++ frontend/src/routes/not-found.component.jsx
@@ -1,10 +1,21 @@
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap';
 
-const NotFound = () => (
-  <Container className='text-center'>
-    <h1>404</h1>
-    <p>Page not found</p>
-  </Container>
-);
+const NotFound = () => {
+  return (
+    <Container fluid>
+      <Container className="d-flex flex-column align-items-center mt-5">
+        <div className="text-center">
+          <h1 className="display-1">404</h1>
+          <p>Oops! That page couldn't be found.</p>
+          <LinkContainer to={'/'}>
+            <Button variant='outline-success' size='lg' className='rounded-pill'>Go to Home</Button>
+          </LinkContainer>
+        </div>
+      </Container>
+    </Container>
+  );
+};
 
 export default NotFound;
